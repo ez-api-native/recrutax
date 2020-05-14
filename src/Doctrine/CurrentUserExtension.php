c42c0f49bbb93c485e447f4bcbb811b5d25132bd
Vodkhard <hugo.knorr@gmail.com>
2020-05-14 21:45:46 +0200

    Current user filter

---
+++ src/Doctrine/CurrentUserExtension.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Doctrine;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use App\Entity\Offer;
+use App\Entity\Submission;
+use App\Entity\User;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\Security\Core\Security;
+
+final class CurrentUserExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
+{
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null): void
+    {
+        $this->addWhere($queryBuilder, $resourceClass, $operationName);
+    }
+
+    public function applyToItem(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, array $identifiers, string $operationName = null, array $context = []): void
+    {
+        $this->addWhere($queryBuilder, $resourceClass);
+    }
+
+    private function addWhere(QueryBuilder $queryBuilder, string $resourceClass, ?string $operationName = null): void
+    {
+        /** @var $user User */
+        if ($this->security->isGranted('ROLE_SUPER_ADMIN') || null === $user = $this->security->getUser()) {
+            return;
+        }
+
+        $rootAlias = $queryBuilder->getRootAliases()[0];
+        if ($resourceClass === Offer::class) {
+            $queryBuilder->andWhere(sprintf('%s.owner = :current_user', $rootAlias));
+            $queryBuilder->setParameter('current_user', $user);
+        }
+        if ($resourceClass === Submission::class && 'api_offers_submissions_get_subresource' !== $operationName) {
+            $queryBuilder->join(sprintf('%s.offer', $rootAlias), 'o');
+            $queryBuilder->expr()->orX(
+                $queryBuilder->expr()->eq('o.owner', ':current_user'),
+                $queryBuilder->expr()->eq(sprintf('%s.candidate', $rootAlias), ':current_user')
+            );
+            $queryBuilder->setParameter('current_user', $user);
+        }
+    }
+}
